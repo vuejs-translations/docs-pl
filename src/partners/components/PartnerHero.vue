19663bc83039ec9a4d121d33d13f2bc2ce791f50
Dion Vogliqi <dionvogliqi@gmail.com>
2024-11-11 03:07:50 +0100

    Improvements to partnership page (#3095)

--- src/partners/components/PartnerHero.vue
+++ src/partners/components/PartnerHero.vue
@@ -8,12 +8,14 @@ defineProps<{ title?: string }>()
   <PageHero>
     <template #title>{{ title || 'Vue Partners' }}</template>
     <template #lead>
-      Partnerzy Vue to zatwierdzone przez zespół Vue agencje, które świadczą najwyższej klasy
-      usługi konsultingowe i programistyczne Vue. Jeśli Twoja firma jest
-      zainteresowana dołączeniem do grona partnerów, prosimy
-      <a class="link" href="https://airtable.com/shrCQhat57SApJI2l" target="_blank"
-        >zarejestruj swoje zainteresowanie tutaj</a
-      >.
+      <p>
+        Partnerzy Vue to zatwierdzone przez zespół Vue agencje, które świadczą najwyższej klasy
+        usługi konsultingowe i programistyczne Vue. Jeśli Twoja firma jest
+        zainteresowana dołączeniem do grona partnerów, prosimy
+        <a class="link" href="https://airtable.com/shrCQhat57SApJI2l" target="_blank"
+          >zarejestruj swoje zainteresowanie tutaj</a
+        >.
+      </p>
     </template>
   </PageHero>
 </template>
