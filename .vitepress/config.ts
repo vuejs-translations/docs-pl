vuejs/docs

Add developers pages, Proxify as a partner (#2985)

.vitepress/config.ts
@@ -45,6 +45,7 @@ const nav: ThemeConfig['nav'] = [
         text: 'Resources',
         items: [
           { text: 'Partners', link: '/partners/' },
+          { text: 'Developers', link: '/developers/' },
           { text: 'Themes', link: '/ecosystem/themes' },
           { text: 'UI Components', link: 'https://ui-libs.vercel.app/' },
           {
@@ -125,9 +126,13 @@ const nav: ThemeConfig['nav'] = [
     link: '/sponsor/'
   },
   {
-    text: 'Partners',
-    link: '/partners/',
-    activeMatch: `^/partners/`
+    text: 'Experts',
+    badge: { text: 'NEW' },
+    activeMatch: `^/(partners|developers)/`,
+    items: [
+      { text: 'Partners', link: '/partners/' },
+      { text: 'Developers', link: '/developers/', badge: { text: 'NEW' } }
+    ]
   }
 ]
 

vuejs-translations/docs-pl

.vitepress/config.ts
@@ -45,6 +45,7 @@ const nav: ThemeConfig['nav'] = [
         text: 'Materiały',
         items: [
           { text: 'Partnerzy', link: '/partners/' },
+          { text: 'Deweloperzy', link: '/developers/' },
           { text: 'Motywy', link: '/ecosystem/themes' },
           { text: 'Komponenty UI', link: 'https://ui-libs.vercel.app/' },
           {
@@ -131,9 +132,13 @@ const nav: ThemeConfig['nav'] = [
     link: '/sponsor/'
   },
   {
-    text: 'Partnerzy',
-    link: '/partners/',
-    activeMatch: `^/partners/`
+    text: 'Eksperci',
+    badge: { text: 'NEW' },
+    activeMatch: `^/(partners|developers)/`,
+    items: [
+      { text: 'Partnerzy', link: '/partners/' },
+      { text: 'Deweloperzy', link: '/developers/', badge: { text: 'NEW' } }
+    ]
   }
 ]
 