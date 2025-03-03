vuejs/docs

3.5: custom elements

.vitepress/config.ts
@@ -421,6 +421,7 @@ export const sidebar: ThemeConfig['sidebar'] = {
     {
       text: 'Advanced APIs',
       items: [
+        { text: 'Custom Elements', link: '/api/custom-elements' },
         { text: 'Render Function', link: '/api/render-function' },
         { text: 'Server-Side Rendering', link: '/api/ssr' },
         { text: 'TypeScript Utility Types', link: '/api/utility-types' },

vuejs-translations/docs-pl

.vitepress/config.ts
@@ -441,6 +441,7 @@ export const sidebar: ThemeConfig['sidebar'] = {
     {
       text: 'Zaawansowany interfejs API',
       items: [
+        { text: 'Niestandardowe elementy', link: '/api/custom-elements' },
         { text: 'Funkcja render', link: '/api/render-function' },
         {
           text: 'Renderowanie po stronie serwera (SSR)',