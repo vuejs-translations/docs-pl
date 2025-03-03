vuejs/docs

3.5: add composition api helpers page

.vitepress/config.ts
@@ -368,6 +368,10 @@ export const sidebar: ThemeConfig['sidebar'] = {
         {
           text: 'Dependency Injection',
           link: '/api/composition-api-dependency-injection'
+        },
+        {
+          text: 'Helpers',
+          link: '/api/composition-api-helpers'
         }
       ]
     },

vuejs-translations/docs-pl

.vitepress/config.ts
@@ -388,6 +388,10 @@ export const sidebar: ThemeConfig['sidebar'] = {
         {
           text: 'Wstrzykiwanie zależności',
           link: '/api/composition-api-dependency-injection'
+        },
+        {
+          text: 'Helpery',
+          link: '/api/composition-api-helpers'
         }
       ]
     },