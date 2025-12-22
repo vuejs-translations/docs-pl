vuejs/docs

feat: add Vue Plugins to the resources list (#3283)

.vitepress/config.ts
@@ -50,6 +50,7 @@ const nav: ThemeConfig['nav'] = [
           { text: 'Developers', link: '/developers/' },
           { text: 'Themes', link: '/ecosystem/themes' },
           { text: 'UI Components', link: 'https://ui-libs.vercel.app/' },
+          { text: 'Plugins Collection', link: 'https://www.vue-plugins.org/' },
           {
             text: 'Certification',
             link: 'https://certificates.dev/vuejs/?ref=vuejs-nav'

vuejs-translations/docs-pl

.vitepress/config.ts
@@ -49,6 +49,7 @@ const nav: ThemeConfig['nav'] = [
           { text: 'Deweloperzy', link: '/developers/' },
           { text: 'Motywy', link: '/ecosystem/themes' },
           { text: 'Komponenty UI', link: 'https://ui-libs.vercel.app/' },
+          { text: 'Kolekcja wtyczek', link: 'https://www.vue-plugins.org/' },
           {
             text: 'Certyfikacja',
             link: 'https://certificates.dev/vuejs/?ref=vuejs-nav'