vuejs/docs

remove developers section

.vitepress/config.ts
@@ -129,13 +129,9 @@ const nav: ThemeConfig['nav'] = [
     link: '/sponsor/'
   },
   {
-    text: 'Experts',
-    badge: { text: 'NEW' },
-    activeMatch: `^/(partners|developers)/`,
-    items: [
-      { text: 'Partners', link: '/partners/' },
-      { text: 'Developers', link: '/developers/', badge: { text: 'NEW' } }
-    ]
+    text: 'Partners',
+    activeMatch: `^/partners/`,
+    link: '/partners/'
   }
 ]
 

vuejs-translations/docs-pl

.vitepress/config.ts
@@ -134,13 +134,9 @@ const nav: ThemeConfig['nav'] = [
     link: '/sponsor/'
   },
   {
-    text: 'Eksperci',
-    badge: { text: 'NEW' },
-    activeMatch: `^/(partners|developers)/`,
-    items: [
-      { text: 'Partnerzy', link: '/partners/' },
-      { text: 'Deweloperzy', link: '/developers/', badge: { text: 'NEW' } }
-    ]
+    text: 'Partnerzy',
+    activeMatch: `^/partners/`,
+    link: '/partners/'
   }
 ]
 