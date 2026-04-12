vuejs/docs

docs: reorganize nav menu structure (#3368)
- Move API into Docs dropdown
- Move Quick Start to top of Docs dropdown
- Group Sponsor and Partners under Support
- Remove Partners duplicate from Ecosystem > Resources

.vitepress/config.ts
@@ -18,12 +18,13 @@ import {
 const nav: ThemeConfig['nav'] = [
   {
     text: 'Docs',
-    activeMatch: `^/(guide|style-guide|cookbook|examples)/`,
+    activeMatch: `^/(guide|tutorial|examples|api|glossary|error-reference)/`,
     items: [
       { text: 'Quick Start', link: '/guide/quick-start' },
       { text: 'Guide', link: '/guide/introduction' },
       { text: 'Tutorial', link: '/tutorial/' },
       { text: 'Examples', link: '/examples/' },
+      { text: 'API', link: '/api/' },
       // { text: 'Style Guide', link: '/style-guide/' },
       { text: 'Glossary', link: '/glossary/' },
       { text: 'Error Reference', link: '/error-reference/' },
@@ -38,11 +39,6 @@ const nav: ThemeConfig['nav'] = [
     ]
   },
   {
-    text: 'API',
-    activeMatch: `^/api/`,
-    link: '/api/'
-  },
-  {
     text: 'Playground',
     link: 'https://play.vuejs.org'
   },
@@ -53,7 +49,6 @@ const nav: ThemeConfig['nav'] = [
       {
         text: 'Resources',
         items: [
-          { text: 'Partners', link: '/partners/' },
           { text: 'Themes', link: '/ecosystem/themes' },
           { text: 'UI Components', link: 'https://ui-libs.vercel.app/' },
           {
@@ -134,13 +129,12 @@ const nav: ThemeConfig['nav'] = [
     ]
   },
   {
-    text: 'Sponsor',
-    link: '/sponsor/'
-  },
-  {
-    text: 'Partners',
-    activeMatch: `^/partners/`,
-    link: '/partners/'
+    text: 'Support',
+    activeMatch: `^/(sponsor|partners)/`,
+    items: [
+      { text: 'Sponsor', link: '/sponsor/' },
+      { text: 'Partners', link: '/partners/' }
+    ]
   }
 ]
 

vuejs-translations/docs-pl

.vitepress/config.ts
@@ -17,12 +17,13 @@ import {
 const nav: ThemeConfig['nav'] = [
   {
     text: 'Dokumentacja',
-    activeMatch: `^/(guide|style-guide|cookbook|examples)/`,
+    activeMatch: `^/(guide|tutorial|examples|api|glossary|error-reference)/`,
     items: [
       { text: 'Szybki start', link: '/guide/quick-start' },
       { text: 'Poradnik', link: '/guide/introduction' },
       { text: 'Samouczek', link: '/tutorial/' },
       { text: 'Przykłady', link: '/examples/' },
+      { text: 'API', link: '/api/' },
       // { text: 'Style Guide', link: '/style-guide/' },
       { text: 'Słownik', link: '/glossary/' },
       { text: 'Referencja błędów', link: '/error-reference/' },
@@ -37,11 +38,6 @@ const nav: ThemeConfig['nav'] = [
     ]
   },
   {
-    text: 'API',
-    activeMatch: `^/api/`,
-    link: '/api/'
-  },
-  {
     text: 'Interaktywne demo',
     link: 'https://play.vuejs.org'
   },
@@ -52,7 +48,6 @@ const nav: ThemeConfig['nav'] = [
       {
         text: 'Materiały',
         items: [
-          { text: 'Partnerzy', link: '/partners/' },
           { text: 'Motywy', link: '/ecosystem/themes' },
           { text: 'Komponenty UI', link: 'https://ui-libs.vercel.app/' },
           {
@@ -139,13 +134,12 @@ const nav: ThemeConfig['nav'] = [
     ]
   },
   {
-    text: 'Sponsorzy',
-    link: '/sponsor/'
-  },
-  {
-    text: 'Partnerzy',
-    activeMatch: `^/partners/`,
-    link: '/partners/'
+    text: 'Wsparcie',
+    activeMatch: `^/(sponsor|partners)/`,
+    items: [
+      { text: 'Sponsorzy', link: '/sponsor/' },
+      { text: 'Partnerzy', link: '/partners/' }
+    ]
   }
 ]
 