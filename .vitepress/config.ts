vuejs/docs

docs: move Quick Start to top of Docs nav menu (#3366)

.vitepress/config.ts
@@ -20,10 +20,10 @@ const nav: ThemeConfig['nav'] = [
     text: 'Docs',
     activeMatch: `^/(guide|style-guide|cookbook|examples)/`,
     items: [
+      { text: 'Quick Start', link: '/guide/quick-start' },
       { text: 'Guide', link: '/guide/introduction' },
       { text: 'Tutorial', link: '/tutorial/' },
       { text: 'Examples', link: '/examples/' },
-      { text: 'Quick Start', link: '/guide/quick-start' },
       // { text: 'Style Guide', link: '/style-guide/' },
       { text: 'Glossary', link: '/glossary/' },
       { text: 'Error Reference', link: '/error-reference/' },

vuejs-translations/docs-pl

.vitepress/config.ts
@@ -19,10 +19,10 @@ const nav: ThemeConfig['nav'] = [
     text: 'Dokumentacja',
     activeMatch: `^/(guide|style-guide|cookbook|examples)/`,
     items: [
+      { text: 'Szybki start', link: '/guide/quick-start' },
       { text: 'Poradnik', link: '/guide/introduction' },
       { text: 'Samouczek', link: '/tutorial/' },
       { text: 'Przykłady', link: '/examples/' },
-      { text: 'Szybki start', link: '/guide/quick-start' },
       // { text: 'Style Guide', link: '/style-guide/' },
       { text: 'Słownik', link: '/glossary/' },
       { text: 'Referencja błędów', link: '/error-reference/' },